67f159a63328e2d104874f37c425920b797712f9
Harold Rojas <cmoharold@gmail.com>
2023-01-08 21:10:07 +0100

    fix: cors config

--- server/server.js
+++ server/server.js
@@ -12,9 +12,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const app = express();
-app.use(cors(
-    config.application.cors.server
-));
+app.use(cors());
 app.use(express.json());
 
 app.get('/', async(req, res) => {
